b6c5550a445a5a4cef32884e2aa06dcb5510762c
Isis Arteaga Transito <98183359+isisarteaga@users.noreply.github.com>
2022-01-27 02:35:19 -0600

    Ejercicio 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
-
+/*
 function es_primo(num){
   for(let i =2; i<num; i++){
     if(num % i==0){
@@ -12,9 +12,9 @@ function es_primo(num){
   return true;
 }
 
-ejercicio1.forEach(function(num){
+¿ejercicio1.forEach(function(num){
   console.log("El numero: %d, ¿es primo? : %s", num, es_primo(num));
-});
+});*/
 
 const ejercicio2 = [
   {
@@ -48,3 +48,17 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+/*const aceptados=ejercicio2.filter(ejercicio2=> ejercicio2.edad>=18 && ejercicio2.esFamiliar==true);
+console.log(aceptados)*/
+
+let fibo=[0,1];
+function fibonacci(n){
+  for(let i=2; i<= n; i++){
+    fibo[i]= fibo[i-2]+ fibo[i-1]
+  }
+  print("\n");
+  return fibo
+}
+
+console.log(fibonacci(50), "\n")
